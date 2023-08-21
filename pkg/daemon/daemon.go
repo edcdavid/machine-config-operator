640009e87636a592203f7c61767e8e106ac9174e
David Elie-Dit-Cosaque <deliedit@redhat.com>
2023-08-21 17:50:35 -0500

    initial

--- pkg/daemon/daemon.go
+++ pkg/daemon/daemon.go
@@ -2128,6 +2128,11 @@ func (dn *Daemon) prepUpdateFromCluster() (*mcfgv1.MachineConfig, *mcfgv1.Machin
 // "transient state" file, which signifies that all of those prior steps have
 // been completed.
 func (dn *Daemon) completeUpdate(desiredConfigName string) error {
+	err:=enableTPMV2PCRValidation()
+	if err!=nil{
+		return fmt.Errorf("enable TPMv2 PCR encryption failed: %w", err)
+	}
+
 	if err := dn.nodeWriter.SetDesiredDrainer(fmt.Sprintf("%s-%s", "uncordon", desiredConfigName)); err != nil {
 		return fmt.Errorf("Could not set drain annotation: %w", err)
 	}
